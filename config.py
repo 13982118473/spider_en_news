d0f29480f2b613e5c27592a55bd87ab6ef8952e1
hl <hl@repackseo.com>
2021-01-22 16:28:33 +0800

    配置文件提交

---
+++ config.py
@@ -0,0 +1,9 @@
+
+#设置数据库
+sql_date={
+    "host": "154.212.112.247",  # 域名
+    "port": "17027",  # 端口
+    "user": "tiupuqg9erb64koe",  # 用户名
+    "passwd": "ckhrde3cqe7ck817",  # 密码
+    "authSource":"admin"
+}
